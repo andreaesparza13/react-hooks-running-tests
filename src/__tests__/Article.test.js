e7dad928eaff18b91a89ea7c7b95d34c2051f0f7
Andrea <acesparza05@gmail.com>
2022-07-17 11:13:14 -0500

    commit tree fbaf5d2f793bf518535c5acb5b80249d16a9fa73

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,27 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   // add this line
+//   screen.debug();
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
